9a697077522d891301ed5fc100ff7d8ecdfb4453
4qwerty7 <4qwerty7@163.com>
2019-05-23 19:27:54 +0800

    fix a bug

--- modules/contest.js
+++ modules/contest.js
@@ -551,7 +551,7 @@ app.get('/contest/submission/:id', async (req, res) => {
       formattedCode: judge.formattedCode ? judge.formattedCode.toString("utf8") : null,
       preferFormattedCode: res.locals.user ? res.locals.user.prefer_formatted_code : false,
       detailResult: processOverallResult(judge.result, displayConfig),
-      socketToken: (judge.pending && x.task_id != null) ? jwt.sign({
+      socketToken: (judge.pending && judge.task_id != null) ? jwt.sign({
         taskId: judge.task_id,
         displayConfig: displayConfig,
         type: 'detail'
